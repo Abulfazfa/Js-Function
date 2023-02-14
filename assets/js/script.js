10b522b421f32037dab4c962d4a966a4bd1bc991
unknown <abulfazfa@code.edu.az>
2023-02-14 17:32:11 +0400

    finish_task

---
+++ assets/js/script.js
@@ -0,0 +1,86 @@
+////////task-1//////////
+// let n = prompt();
+// let func1 = (n) =>{
+//     if(n % 21 == 0){
+//         console.log(true);
+//     }
+//     else{
+//         console.log(false);
+//     }
+// }
+// func(n);
+////////////////////////
+
+////////task-2/////////
+// let m = prompt();
+// let factorial = 1;
+// let func2 = (n) =>{
+//     for(let i = 1; i <= n; i++){
+//         factorial = factorial * i;
+//     }
+//     console.log(sum);
+// }
+// func2(m);
+////////////////////////
+
+////////task-3/////////
+// let k = [1,2,3,4,5,6,7,8,9];
+// let square = 0;
+// let func3 = ([]) =>{
+//     for(let i = 0; i < k.length; i++){
+//         if(k[i]%2 == 0){
+//             square = square + k[i]*k[i];
+//         }
+//     }
+//     console.log(square);
+// }
+// func3(k);
+////////////////////////
+
+////////task-4/////////
+// let email = prompt("Email");
+// let password = prompt("Password");
+// let func4 = (email, password) =>{
+//     if(email == "lorem@code.edu.az" && password == "12345"){
+//         document.writeln("<h1>Girish olundu</h1>");
+//     }
+//     else{
+//         document.writeln("<h1>Email ve yaxud password sehvdi</h1>");
+//     }
+// }
+// func4(email, password);
+////////////////////////
+
+////////task-5/////////
+// let sum = 0;
+// let arr = [];
+// for (let i = 0; i < 5; i++) {
+//     arr[i]  = parseInt(prompt());
+// }
+// let func5 = ([]) =>{
+//     for (let i = 0; i < arr.length; i++) {
+//         if(arr[i]%2 == 0){
+//            sum = sum + arr[i];
+//         }
+//     }
+//     console.log(sum);
+// }
+// func5(arr);
+///////////////////////
+
+////////task-6/////////
+// let count = 0;
+// let arr = [];
+// for (let i = 0; i < 5; i++) {
+//     arr[i]  = parseInt(prompt());
+// }
+// let func5 = ([]) =>{
+//     for (let i = 0; i < arr.length; i++) {
+//         if(arr[i]%2 == 0){
+//            count ++;
+//         }
+//     }
+//     console.log(count);
+// }
+// func5(arr);
+///////////////////////
